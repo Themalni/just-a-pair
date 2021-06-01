f1ae223d132143d169bcbc0c6a368eed0abdc164
Themalni <themalni@live.co.uk>
2021-06-01 14:30:46 +0200

    separated store into modules

--- src/firebaseAuth.js
+++ src/firebaseAuth.js
@@ -1,4 +1,4 @@
-import { store } from '@/store';
+import { store } from './store';
 import { fb } from "@/firebase";
 
 export default {
---
+++ src/store/getters.js
@@ -0,0 +1,10 @@
+export default {
+    products: state => state.products,
+    gridImages: state => state.gridImages,
+    sizeGuideMen: state => state.sizeGuideMen,
+    sizeGuideWomen: state => state.sizeGuideWomen,
+    sizeGuideChildren: state => state.sizeGuideChildren,
+    inCart: state => state.inCart,
+    productPreview: state => state.productPreview,
+    user: state => state.user
+}
