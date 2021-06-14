2d132388d190998c3ad8bf6ad1cd126e809ce88f
Themalni <themalni@live.co.uk>
2021-06-14 21:30:52 +0200

    moved export to filter

--- src/filters.js
+++ src/filters.js
@@ -1,9 +1,7 @@
 import Vue from 'vue';
 
-Vue.filter('salePrice', (price, sale) => {
+export default Vue.filter('salePrice', (price, sale) => {
     if (!price) price = 0;
     if (!sale) sale = 0;
     return price - Math.floor((sale / 100) * price);
 });
-
-export default Vue.filter('salePrice');
